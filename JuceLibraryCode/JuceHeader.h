07ba8d19cc7fe24cf5774a20dfb3c29ad7e0d2d2
Geert Bevin <gbevin@uwyn.com>
2017-01-14 11:19:34 +0100

    Set version to 1.0.1

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -35,8 +35,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "sendmidi";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  versionString  = "1.0.1";
+    const int          versionNumber  = 0x10001;
 }
 #endif
 

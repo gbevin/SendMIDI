af8dd318b34b929a3eb9a2f5e60b311e40887c8d
Geert Bevin <gbevin@uwyn.com>
2017-01-16 01:03:24 +0100

    Updated version to 1.0.4

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -35,8 +35,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "sendmidi";
-    const char* const  versionString  = "1.0.3";
-    const int          versionNumber  = 0x10003;
+    const char* const  versionString  = "1.0.4";
+    const int          versionNumber  = 0x10004;
 }
 #endif
 

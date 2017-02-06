a2db2cdcfe1e1d7530bb5087d2adcd513c0690b7
Geert Bevin <gbevin@uwyn.com>
2017-02-06 10:19:10 +0100

    Updated to v1.0.6

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -32,8 +32,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "sendmidi";
-    const char* const  versionString  = "1.0.5";
-    const int          versionNumber  = 0x10005;
+    const char* const  versionString  = "1.0.6";
+    const int          versionNumber  = 0x10006;
 }
 #endif
 

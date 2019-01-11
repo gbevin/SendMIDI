de51008cafab85c2d03b6fd3da7379b3b2887af8
Geert Bevin <gbevin@uwyn.com>
2019-01-11 10:24:21 -0500

    Updated copyright year

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of SendMIDI.
- * Copyright (command) 2017 Uwyn SPRL.  http://www.uwyn.com
+ * Copyright (command) 2017-2019 Uwyn SPRL.  http://www.uwyn.com
  *
  * SendMIDI is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

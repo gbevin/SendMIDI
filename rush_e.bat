89fc3f71f94650252f752487a0652830b376b901
Samuel Parente <107854126+par-sam@users.noreply.github.com>
2022-07-02 22:01:47 +0200

    Added an example to play Rush E

---
+++ rush_e.bat
@@ -0,0 +1,316 @@
+set midi_output="Roland Digital Piano"
+
+sendmidi dev %midi_output% on 64 70 on 45 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70 on 48 70 on 52 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70
+
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70 on 40 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70 on 48 70 on 52 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70 on 45 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70 on 48 70 on 52 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70 on 40 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70 on 48 70 on 52 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70 on 45 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70 on 48 70 on 52 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70 on 40 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 65 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70 on 48 70 on 52 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 63 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 64 70 on 45 70
+
+for /L %%a In (0 1 50) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 69 70 on 48 70 on 52 70
+
+for /L %%a In (0 1 50) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 72 70 on 40 70
+
+for /L %%a In (0 1 50) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 48 70 on 52 70
+
+for /L %%a In (0 1 50) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 74 70 on 44 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 74 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 74 70 on 50 70 on 52 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 74 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 74 70 on 40 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 72 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 71 70 on 50 70 on 52 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 74 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 72 70 on 45 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 72 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 72 70 on 48 70 on 52 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 72 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 72 70 on 40 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 71 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 69 70 on on 48 70 on 52 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 72 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 71 70 on 47 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 71 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 71 70 on 51 70 on 54 70 on 57 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 71 70
+
+for /L %%a In (0 1 20) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 66 70 on 42 70
+
+for /L %%a In (0 1 50) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 71 70 on 51 70 on 54 70 on 57 70
+
+for /L %%a In (0 1 50) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 40 70 on 68 70
+
+for /L %%a In (0 1 50) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 47 70
+
+for /L %%a In (0 1 50) do (
+    ping -n 1 -w 1.1.1.1 > nul
+)
+
+sendmidi dev %midi_output% on 52 70 on 56 70 on 59 70 on 64 70

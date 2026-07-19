98e90d8e32757c7e211dadf40dce155fc7bdf0c0
Geert Bevin <gbevin@uwyn.com>
2026-07-18 22:01:57 -0400

    Updated version to 1.4.3

--- Tests/JuceLibraryCode/JuceHeader.h
+++ Tests/JuceLibraryCode/JuceHeader.h
@@ -42,7 +42,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "SendMIDITests";
     const char* const  companyName    = "Uwyn";
-    const char* const  versionString  = "1.4.2";
-    const int          versionNumber  = 0x10402;
+    const char* const  versionString  = "1.4.3";
+    const int          versionNumber  = 0x10403;
 }
 #endif
